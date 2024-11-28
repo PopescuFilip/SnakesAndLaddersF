40f98dc8c2d0f86d7b11097f3ad4075495ffb36d
Alexandru Pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-11-28 23:34:14 +0200

    DiceValue enum and dice pictures

---
+++ frontend/models/DiceValue.h
@@ -0,0 +1,30 @@
+//
+// Created by Alexandru Pestritu on 28.11.2024.
+//
+
+#ifndef DICEVALUE_H
+#define DICEVALUE_H
+
+enum class DiceValue {
+    DICE_1,
+    DICE_2,
+    DICE_3,
+    DICE_4,
+    DICE_5,
+    DICE_6
+};
+
+
+inline QString getMapImagePath(const DiceValue& diceValue) {
+    switch (diceValue) {
+        case DiceValue::DICE_1: return ":/images/dice-1.png";
+        case DiceValue::DICE_2: return ":/images/dice-2.png";
+        case DiceValue::DICE_3: return ":/images/dice-3.png";
+        case DiceValue::DICE_4: return ":/images/dice-4.png";
+        case DiceValue::DICE_5: return ":/images/dice-5.png";
+        case DiceValue::DICE_6: return ":/images/dice-6.png";
+    }
+    return {};
+}
+
+#endif //DICEVALUE_H
